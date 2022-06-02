ea0d7f05c4f0172c100bebc1445198f7dabce572
- <>
2022-06-02 17:44:45 +0300

    models

--- mysite/point_site/models.py
+++ mysite/point_site/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.TextField()
+    password = models.TextField()
+    points = models.IntegerField() 
