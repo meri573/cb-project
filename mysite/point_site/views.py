7ac7f958ef7ee852904c80c9d3184136ec0593ff
- <>
2022-06-02 18:22:58 +0300

    started templates

--- mysite/point_site/views.py
+++ mysite/point_site/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homePageView(request):
-    return HttpResponse("Hello world")
+    return render(request, 'point_site/index.html')
+
+
+def loginView(request):
+    pass
