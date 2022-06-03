c9ca362128068e29ef041c5b6116caeb8e81deb5
- <>
2022-06-03 16:37:04 +0300

    login

--- mysite/point_site/views.py
+++ mysite/point_site/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def homePageView(request):
     return render(request, 'point_site/index.html')
 
