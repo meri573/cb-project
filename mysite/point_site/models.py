f7d86d1c65680c2c335370b8aa007df7e141f13c
- <>
2022-06-03 17:08:37 +0300

    models

--- mysite/point_site/models.py
+++ mysite/point_site/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
-class User(models.Model):
-    name = models.TextField()
-    password = models.TextField()
-    points = models.IntegerField() 
+class Points(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = models.IntegerField()
